d26a125499d6da8895fa2766845f9b903b479f51
jlmooreiii <jloftonmooreiii@gmail.com>
2020-08-03 08:22:02 -0700

    finished week5 assessment

--- code-challenges.js
+++ code-challenges.js
@@ -7,8 +7,51 @@ var secretCodeWord1 = "lackadaisical"
 var secretCodeWord2 = "gobbledygook"
 // Expected output: "g0bbl3dyg00k"
 
-
-
+// Declare a function that takes a string as param
+const codeMyWord = (string) => {
+    // Check to make sure the input is actually a string
+    if (typeof string !== "string") {
+        return "Please enter a string as a parameter to the function codeMyWord()"
+    } else {
+        // Declare a hash set to serve as a key
+        var myKey = {
+            a: "4",
+            A: "4",
+            e: "3",
+            E: "3",
+            i: "1",
+            I: "1",
+            o: "0",
+            O: "0",
+        }
+        // Convert the string into an array of characters
+        let charArray = string.split("");
+        // Iterate through the array of lettes and check against the key.
+        let codedCharArray = charArray.map(value => {
+            // If one of the letters is in the key, then convert the letter into the number associates
+            if (myKey.hasOwnProperty(value)) {
+            // if (Object.keys(myKey).includes(value)) {
+                return myKey[value];
+            } else {
+                return value;
+            }
+        })
+        // Combine the array of characters to create the new coded string
+        newCodedWord = codedCharArray.join("");
+        // Return the coded string
+        return newCodedWord;
+      }
+    }
+    //Test to make sure that the method works
+    console.log("-----CHALLENGE 1-----");
+    console.log("This the the original variable: secretCodeWord1 = lackadaisical")
+    console.log("This is the result of the method:");
+    console.log(codeMyWord(secretCodeWord1));
+    console.log("\n");
+    console.log("This the the original variable: secretCodeWord2 = gobbledygook")
+    console.log("This is the result of the method:");
+    console.log(codeMyWord(secretCodeWord2));
+    console.log("\n");
 
 
 // --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
@@ -16,9 +59,22 @@ var secretCodeWord2 = "gobbledygook"
 var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
 // Expected output: "Apple" "Banana" "Peach"
 
-
-
-
+// Declare a function that will take in any array
+const wordsWithA = (array) => {
+    // Use filter. For each element in the array, first check to make sure that the element is a string
+    // If the element is a string, make sure the character "a" is in the string
+    let newArray = arrayOfWords.filter(value => {
+       return (typeof value == "string") && 
+        ((value.includes("a")) || value.includes("A"))
+    })
+    return newArray;
+}
+//Test to make sure that the method works
+console.log("-----CHALLENGE 2-----");
+console.log("This the the original variable: arrayOfWords = ['Apple', 'Banana', 'Plum', 'Cherry', 'Kiwi', 'Peach']")
+console.log("This is the result of the method:");
+console.log(wordsWithA(arrayOfWords));
+console.log("\n");
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
@@ -28,3 +84,58 @@ var hand2 = [5, 5, 3, 3, 4]
 // Expected output: false
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
+
+// Declare a function that takes in an array 
+const isFullHouse = (array) => {
+    // Declare booleans to track if the full house conditions are met
+    let threeOfAKinda = false;
+    let pair = false;
+    // As I'm doing my comparison, need to make sure that I'm not checking elements whose values I have already been checked
+    let valuesAlreadyChecked = [];
+    
+    // Create a loop for an outter count
+    for (let x = 0; x < array.length; x++) {
+        // Need to make sure that I'm not checking elements whose values I have already been checked
+        if (valuesAlreadyChecked.includes(array[x])){
+            valuesAlreadyChecked = valuesAlreadyChecked;
+
+        //If the value is novel, then proceed with logic
+        } else {
+            valuesAlreadyChecked.push(array[x]);
+            // This is the secret sauce variable right here. This keeps track of how often a value has come up
+            let matchCount = 0;
+            // Now I need to creat a loop so that each element in the outter loop is compared to every following element in the loop
+            for (let y = x+1; y < array.length; y++) {
+                if (array[x] == array[y]) {
+                    matchCount++;
+                }
+            }
+
+            // Update the booleans if necessary
+            if (matchCount == 2) {threeOfAKinda = true;} 
+            if (matchCount == 1) {pair = true;}
+
+        }
+    }
+    // Check the conditions to see if I have a full house
+    if (threeOfAKinda == true && pair == true) {
+        return true;
+    } else {
+        return false;
+      }
+}
+//Test to make sure that the method works
+console.log("-----CHALLENGE 3-----");
+console.log("This the the original variable: hand1 = [5, 5, 5, 3, 3]")
+console.log("This is the result of the method:");
+console.log(isFullHouse(hand1));
+console.log("\n");
+console.log("This the the original variable: hand2 = [5, 5, 3, 3, 4]")
+console.log("This is the result of the method:");
+console.log(isFullHouse(hand2));
+console.log("\n");
+console.log("This the the original variable: hand3 = [5, 5, 5, 5, 4]")
+console.log("This is the result of the method:");
+console.log(isFullHouse(hand3));
+console.log("\n");
+
--- rails-questions.md
+++ rails-questions.md
@@ -2,33 +2,51 @@
 
 1. What is the command to create a new Rails application with a Postgres database?
 
+rails new <app_name> -d postregsql -T
+
 
 
 2. Explain each section of the following route:  get '/library/:id' => 'book#show'
 
-get -
-library -
-:id -
-book -
-show -
+get - this is the key word telling rails that you are about to link a non-root page to a method in your main controller
+library - represents a page on the website. Thi sis what the user would type into the URL to help navigate to the desired page
+:id - This is a param call. The param is referenced in the main controller, which will be linked to an html.erb file with direction son how to hande it.
+book - name of the class in the main controller which contains the method to handle how the url typed is handled.
+show - name of the method in the book class that will handle the url that the user inputs.
 
 
 3. Oops, I forgot to add a foreign key to my model. Describe the steps to remedy this mistake.
 
+    You will have to declare a migration. To do this, you need to...
+    1) in the terminal, generate a new migration using the command
+        $ rails generate migration <name_of_migration_file>
+    2) go to the mirgration file located in ./db/migrate
+    3) create a change method and add the desired column:
+    
+    def change
+        add_column :table_name, :column_name, :data_type
+    end
+    4) in the terminal, use the command $ rails db:migrate to update the schema.
 
 
 
 
 4. What is the command to generate a Rails model for a person table with columns named shirt, pants, and shoes?
 
-
-
-
+    $ rails generate model Person shirt:string pants:string shoes:string
 
 5. I want to add validations to the model from question 4 to ensure no one can make a new entry without data in all the columns. How do I do that?
 
-
-
-
+    Inside of the person.rb file, inside the class add the line - 
+    validates :shirt, :pants, :shoes, presence:true
 
 6. What is the naming convention for generating a Rails model? What is the naming convention for the table that is generated with this command?
+
+    The naming convention for genreating a Rails model is singular and pascal cased.
+
+
+    If you are referening this table is stright SQL, then tables is lower cased and plural, snake_cased too if necessary.
+
+    If you are referencing a table in a migration method, the table is lower case, pluralm and snake cased.
+
+    If you are referencing this table as a route or defining relationships in classes, the tables are lower cased and either singular or plural depending on the context. 
